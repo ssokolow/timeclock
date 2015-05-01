9765191f153373d532254798e20aa4d8f08435da
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2015-04-30 20:58:13 -0400

    Generalize the time-specifying code in bedtime_enforcer.py

--- timeclock/controllers/bedtime_enforcer.py
+++ timeclock/controllers/bedtime_enforcer.py
@@ -11,13 +11,19 @@ import gobject, pango
 
 from ..ui.util import MultiMonitorOSD
 
+BEDTIME = 0
+
 class BedtimeEnforcer(gobject.GObject):
     """Very early draft to enforce a sleep cycle.
 
     @todo: Use a multi-process model so that quitting Timeclock doesn't
            dismiss the nag and it is just respawned if it's killed while
            active.
-   """
+    @todo: Once I've moved the input idleness detection into core and added
+           a joystick backend, add an option to automatically adjust the
+           time when it starts nagging in order to learn the correct
+           tunings to produce the desired sleep cycle.
+    """
     def __init__(self, model):
         super(BedtimeEnforcer, self).__init__()
         self.model = model
@@ -36,7 +42,17 @@ class BedtimeEnforcer(gobject.GObject):
             self.last_tick = now
 
             # TODO: Make this configurable
-            if 14 <= datetime.fromtimestamp(now).hour <= 22:
+            hour = datetime.fromtimestamp(now).hour
+            waketime = (BEDTIME + 6) % 24
+
+            # TODO: Is there a more elegant way to do this?
+            alerting = False
+            if waketime > BEDTIME and BEDTIME <= hour < waketime:
+                alerting = True
+            elif waketime < BEDTIME and (BEDTIME <= hour or hour < waketime):
+                alerting = True
+
+            if alerting:
                 self.osd.message("Go The @#$% To Sleep!", -1)
             else:
                 self.osd.hide()
